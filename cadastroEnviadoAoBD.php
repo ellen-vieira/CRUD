cc34c9c76eb4ddfabfafeefd99d6974867146268
EllenVieira1 <vieiraellen755@gmail.com>
2024-11-14 17:27:10 -0300

    Changes

--- cadastroEnviadoAoBD.php
+++ cadastroEnviadoAoBD.php
@@ -11,20 +11,34 @@
     $sql = "select * from alunos where rgm=$rgm";
     $consulta = mysqli_query($ret, $sql);
     $retorno = mysqli_num_rows($consulta);
-
-    if ($retorno > 0)
-        echo "Esse aluno já existe";
-    else{
-        $sql = "insert into alunos (rgm, nome, telefone, sexo) values ('$rgm', '$nome', '$telefone', '$sexo')";
-        $consulta = mysqli_query($ret,$sql);
-        if($consulta)
-            echo " Cadastro realizado com sucesso! ";
-        else
-            echo " Problema ao realizar o cadastro!";
-    }
-    } catch (Exception $e) {
-        echo 'Falha na conexão: '.$e->getMessage();
-    }
-    echo '<br>';
-    echo '<a href="formCadastroBD.php">Voltar ao Cadastro</a>';
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Cadastro</title>
+</head>
+<body>
+    <div class="container">
+        <?php
+            if ($retorno > 0)
+                echo "Esse aluno já existe";
+            else{
+                $sql = "insert into alunos (rgm, nome, telefone, sexo) values ('$rgm', '$nome', '$telefone', '$sexo')";
+                $consulta = mysqli_query($ret,$sql);
+                if($consulta)
+                    echo "Cadastro realizado com sucesso!";
+                else
+                    echo "Problema ao realizar o cadastro!";
+            }
+            } catch (Exception $e) {
+                echo 'Falha na conexão: '.$e->getMessage();
+            }  
+            echo '<a href="formCadastroBD.php"><button>Voltar</button></a>';
+        ?>
+    </div> 
+</body>
+</html>
+
--- excluir.php
+++ excluir.php
@@ -8,14 +8,28 @@
     $sql = "delete from alunos where rgm = $rgm";
     $con = conectar('localhost', 'root','','backend');
     mysqli_query($con, $sql) or die(mysqli_error($con));
-    // Verifica se a exclusão foi efetivada
-    if(mysqli_affected_rows($con) > 0){
-        echo "Aluno excluído com sucesso";
-    }else
-        echo "Aluno NÃO excluído II ";
-        echo '<br>';
-        echo '<a href="informaRGMpExcluir.php"> Voltar a tela de Exclusão </a>'. '<br><br>';
-        echo '<a href="index.php"> Voltar ao Menu </a>';
-    // Outra forma p chamar um programa Php
-    // header("Location: informaRGMpExcluir.php"); 
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Excluuir</title>
+</head>
+<body>
+    <div class="container">
+        <?php
+            // Verifica se a exclusão foi efetivada
+            if(mysqli_affected_rows($con) > 0){
+                echo "Aluno excluído com sucesso";
+            }else
+                echo "Aluno não excluído";
+                echo '<a href="informaRGMpExcluir.php"><button>Voltar</button></a>';
+                echo '<a href="index.php"><button>Voltar ao menu</button></a>';
+            // Outra forma p chamar um programa Php
+            // header("Location: informaRGMpExcluir.php"); 
+        ?>
+    </div>
+</body>
+</html>
